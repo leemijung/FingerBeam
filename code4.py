ad607a82c54f36381dbd16fdf07fdf6a7d00cd87
leemijung <kongs5056@naver.com>
2021-05-12 18:07:07 +0900

    turtle window

--- code4.py
+++ code4.py
@@ -7,6 +7,9 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtWidgets
 
+from PyQt5 import QtGui
+import threading
+from PyQt5 import QtCore
 
 global file
 file = 0
@@ -27,6 +30,7 @@ form_3, base_3 = uic.loadUiType(note_)
 askingsave_ = 'askingsave.ui'
 form_4, base_4 = uic.loadUiType(note_)
 
+
 class convert_file():
     def __init__(self):
         super().__init__()
@@ -46,14 +50,14 @@ class convert_file():
 
     def del_eps(self):
         global cnt
-        for index in range(0, cnt+1):
+        for index in range(0, cnt + 1):
             del_file_name = './img/' + str(index) + '.eps'
             if os.path.isfile(del_file_name):
                 os.remove(del_file_name)
 
     def del_png(self):
         global cnt
-        for index in range(0, cnt+1):
+        for index in range(0, cnt + 1):
             del_file_name1 = './img/' + str(index) + '.png'
             del_file_name2 = './img/' + str(index) + '_.png'
             if os.path.isfile(del_file_name1):
@@ -67,7 +71,7 @@ class convert_file():
         img_list = []
         k = 0
         for i in range(0, cnt):
-            img = Image.open('./'+str(k)+'_.png')
+            img = Image.open('./' + str(k) + '_.png')
             img_1 = img.convert('RGB')
             if k != 0:
                 img_list.append(img_1)
@@ -92,26 +96,26 @@ class take_note():
         return img_color
 
     def save_pen(self, name):
-        pointer.ht()
-        screen.tracer(False)
-        canvas = screen.getcanvas()
-        canvas.postscript(file=name + '.eps', width=wt, height=ht)
+        note_page.pointer.ht()
+        note_page.screen.tracer(False)
+        canvas = note_page.screen.getcanvas()
+        canvas.postscript(file=name + '.eps', width=note_page.wt, height=note_page.ht)
         img = Image.open(name + '.eps')
         img.save('./img/' + name + '_.png')
-        screen.tracer(True)
-        pointer.st()
+        note_page.screen.tracer(True)
+        note_page.pointer.st()
 
     def move_page(self, index):
         t = "./img/" + str(index) + '.png'
         self.clear()
-        win.bgpic(t)
-        win.update()
+        note_page.win.bgpic(t)
+        note_page.win.update()
 
     def clear(self):
-        list_ball_location.clear()
-        history_ball_locations.clear()
-        t1.clear()
-        pointer.clear()
+        note_page.list_ball_location.clear()
+        note_page.history_ball_locations.clear()
+        note_page.t1.clear()
+        note_page.pointer.clear()
 
 
 class opening_page(base_1, form_1):
@@ -154,7 +158,8 @@ class note_page(base_3, form_3):
         self.pushButton_3.clicked.connect(self.home)
         self.pushButton_2.clicked.connect(self.prev)
         # self.pushButton.clicked.connect(self.change)
-        self.pushButton.clicked.connect(self.changeColor)
+        # self.pushButton.clicked.connect(self.changeColor)
+        self.pushButton.clicked.connect(self.start)
         self.pushButton.setCheckable(True)
         self.pushButton.setStyleSheet("background-color : lightblue")
         self.update()
@@ -163,11 +168,118 @@ class note_page(base_3, form_3):
         self.pushButton_5.clicked.connect(self.end)
         # self.label.setText("※ v : 누르면서 필기하세요 ")
 
+    def runa(self):
+
+        print("runa")
+        image1 = Image.open('./img/0.png')
+        wt = image1.size[0]
+        ht = image1.size[1]
+
+        win = t.Screen()
+        screen = t.Screen()
+        win.setup(wt, ht)
+        screen.setup(wt, ht)
+        win.bgpic("./img/0.png")
+
+        cap = cv.VideoCapture(0)
+
+        list_ball_location = []
+        history_ball_locations = []
+        isDraw = True
+
+        t1 = t.Turtle()
+        pointer = t.Turtle()
+        pointer.shape('circle')
+
+        t1.ht()
+        pointer.st()
+        t1.penup()
+        pointer.penup()
+
+        t1.color('blue')
+        pointer.color('red')
+
+        t1.pensize(3)
+        pointer.shapesize(0.5)
+
+        t1.speed(0)
+        pointer.speed(0)
+
+        # global x
+        # global y
+        # global s
+        # global end
+        # global file
+        x = y = s = end = file = 0
+        win.update()
+
+        while True:
+            t_n = take_note()
+            ret, img_color = cap.read()
+            img_color = cv.flip(img_color, 1)
+            img_hsv = cv.cvtColor(img_color, cv.COLOR_BGR2HSV)
+            hue_red = 5
+            lower_red = (hue_red - 5, 95, 165)
+            upper_red = (hue_red + 5, 255, 255)
+            img_mask = cv.inRange(img_hsv, lower_red, upper_red)
+            kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+            img_mask = cv.morphologyEx(img_mask, cv.MORPH_DILATE, kernel, iterations=3)
+            nlabels, labels, stats, centroids = cv.connectedComponentsWithStats(img_mask)
+            max = -1
+            max_index = -1
+
+            for i in range(nlabels):
+                if i < 1:
+                    continue
+                area = stats[i, cv.CC_STAT_AREA]
+                if area > max:
+                    max = area
+                    max_index = i
+
+            if max_index != -1:
+                center_x = int(centroids[max_index, 0])
+                center_y = int(centroids[max_index, 1])
+                gcx = (wt / 640) * center_x
+                gcy = (ht / 480) * center_y
+                cv.circle(img_color, (center_x, center_y), 5, (0, 255, 0), -1)
+                pointer.goto(gcx - (wt / 2), (ht - gcy) - (ht / 2))
+
+                if isDraw:
+                    list_ball_location.append((center_x, center_y))
+                    t1.goto(gcx - (wt / 2), (ht - gcy) - (ht / 2))
+                    t1.pendown()
+                else:
+                    history_ball_locations.append(list_ball_location.copy())
+                    list_ball_location.clear()
+                    t1.penup()
+
+            img_color = t_n.draw_ball_location(img_color, list_ball_location)
+
+            for ball_locations in history_ball_locations:
+                img_color = t_n.draw_ball_location(img_color, ball_locations)
+
+            cv.imshow('Result', img_color)
+
+            key = cv.waitKey(1) & 0xFF
+            if key == 32:  # space bar 누르면 모두 지우기
+                t_n.clear()
+            elif key == ord('v'):  # v 누르면 필기 시작 / 필기 중지
+                isDraw = not isDraw
+
+
+
+    def start(self):
+        global running
+        running = True
+        th = threading.Thread(target=self.runa)
+        th.start()
+        print("started..")
+
     def changeColor(self):
         if self.pushButton.isChecked():
             self.pushButton.setStyleSheet("background-color : #ff557f")
             self.pushButton.setText("필기 중단")
-            #self.change()
+            # self.change()
         else:
             self.pushButton.styleSheet("background-color : lightblue")
 
@@ -224,104 +336,16 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     op = opening_page()
     op.show()
-    #sys.exit(app.exec_())
+    # sys.exit(app.exec_())
     app.exec_()
 
 
-image1 = Image.open('./img/0.png')
-wt = image1.size[0]
-ht = image1.size[1]
-
-win = t.Screen()
-screen = t.Screen()
-win.setup(wt, ht)
-screen.setup(wt, ht)
-win.bgpic("./img/0.png")
-
-cap = cv.VideoCapture(0)
-
-list_ball_location = []
-history_ball_locations = []
-isDraw = True
-
-t1 = t.Turtle()
-pointer = t.Turtle()
-pointer.shape('circle')
-
-t1.ht()
-pointer.st()
-t1.penup()
-pointer.penup()
-
-t1.color('blue')
-pointer.color('red')
-
-t1.pensize(3)
-pointer.shapesize(0.5)
-
-t1.speed(0)
-pointer.speed(0)
-
-x = y = s = end = file = 0
-win.update()
 
 
-while True:
-    t_n = take_note()
-
-    ret, img_color = cap.read()
-    img_color = cv.flip(img_color, 1)
-    img_hsv = cv.cvtColor(img_color, cv.COLOR_BGR2HSV)
-    hue_red = 5
-    lower_red = (hue_red - 5, 95, 165)
-    upper_red = (hue_red + 5, 255, 255)
-    img_mask = cv.inRange(img_hsv, lower_red, upper_red)
-    kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
-    img_mask = cv.morphologyEx(img_mask, cv.MORPH_DILATE, kernel, iterations=3)
-    nlabels, labels, stats, centroids = cv.connectedComponentsWithStats(img_mask)
-    max = -1
-    max_index = -1
-
-    for i in range(nlabels):
-        if i < 1:
-            continue
-        area = stats[i, cv.CC_STAT_AREA]
-        if area > max:
-            max = area
-            max_index = i
-
-    if max_index != -1:
-        center_x = int(centroids[max_index, 0])
-        center_y = int(centroids[max_index, 1])
-        gcx = (wt/640)*center_x
-        gcy = (ht/480)*center_y
-        cv.circle(img_color, (center_x, center_y), 5, (0, 255, 0), -1)
-        pointer.goto(gcx - (wt/2), (ht - gcy) - (ht/2))
-
-        if isDraw:
-            list_ball_location.append((center_x, center_y))
-            t1.goto(gcx - (wt/2), (ht - gcy) - (ht/2))
-            t1.pendown()
-        else:
-            history_ball_locations.append(list_ball_location.copy())
-            list_ball_location.clear()
-            t1.penup()
-
-    img_color = t_n.draw_ball_location(img_color, list_ball_location)
-
-    for ball_locations in history_ball_locations:
-        img_color = t_n.draw_ball_location(img_color, ball_locations)
-
-    cv.imshow('Result', img_color)
-
-    key = cv.waitKey(1) & 0xFF
-    if key == 32:  # space bar 누르면 모두 지우기
-        t_n.clear()
-    elif key == ord('v'):  # v 누르면 필기 시작 / 필기 중지
-        isDraw = not isDraw
 
 
-cap.release()
+
+note_page.cap.release()
 cv.destroyAllWindows()
 
-win.mainloop()
+note_page.win.mainloop()
